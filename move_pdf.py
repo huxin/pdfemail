c4a6d1af36eb28394cf73e78a2d1d3d46291382c
Xin Hu <huxin@pinterest.com>
2017-09-09 18:15:13 -0700

    move

---
+++ move_pdf.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import shutil
+
+if len(sys.argv) < 4:
+    print "Usage:", sys.argv[0], "<src dir> <dst_dir> <cnt>"
+    exit(1)
+
+src_dir, dst_dir, mov_cnt = sys.argv[1:4]
+
+cnt = 0
+for f in os.listdir(src_dir):
+    if not f.endswith('.pdf'):
+        continue
+
+    src_full = os.path.join(src_dir, f)
+    dst_full = os.path.join(dst_dir, f)
+
+    shutil.move(src_full, dst_full)
+    cnt += 1
+
+    if cnt >= mov_cnt:
+        break
+
